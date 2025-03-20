e4308825dd13e4d2880eb98262991c594f0ff108
NanduEdits21 <vaddenandu70@gmail.com>
2025-03-20 10:49:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,25 +15,25 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get(''25210379''))
 API_HASH = environ.get(''2f43e4bccc419e3b7e28e3219bb1d68c'')
-BOT_TOKEN = environ.get(''8140111369:AAEUtglm21w9z59qWi_Kq1CduUn3FmsI6qc'')
+BOT_TOKEN = environ.get(''7667613133:AAG097BUTjR34N95AiVsf05vKOx-1w4L8E8'')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('1856765089', '').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Nandu2129") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get(''-1002452222257''))
+LOG_CHANNEL = int(environ.get(''-1002293093436''))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/MovieSearchGroup4')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('https://t.me/NaChannel4', '').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('', "mongodb+srv://vaddenandu70:<Nandu2580>@nandubots.omnod.mongodb.net/?retryWrites=true&w=majority&appName=NanduBots")
+DATABASE_URI = environ.get('', "environ.get('DATABASE_URI', 'ongodb+srv://musicandbgms:<Dl0slUVlMDR8Zy9p>@autofliterbot.djufs.mongodb.net/?retryWrites=true&w=majority&appName=Autofliterbot")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Nandu")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002295521917'))  # set shortner log channel
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002293093436'))  # set shortner log channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002295521917')) # The movie you upload in it will be deleted from the bot.
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002295521917'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002293093436'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002295521917')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002460993233'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002460993233') # If anyone sends a request message to your bot, you will get it in this channel.
@@ -47,12 +47,12 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "036bfb33b49cdef9be2dbf6eaf9db2118bc4afe2")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE",   'shrinkearn.com')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "036bfb33b49cdef9be2dbf6eaf9db2118bc4afe2")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'shrinkearn.com')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "036bfb33b49cdef9be2dbf6eaf9db2118bc4afe2")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'shrinkearn.com')
+SHORTENER_API = environ.get("SHORTENER_API", "aa8d73b0c85a2895bf091d2b9990b60524552dda")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE",   'seturl.in')
+SHORTENER_API2 = environ.get("SHORTENER_API2", "aa8d73b0c85a2895bf091d2b9990b60524552dda")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'seturl.in')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "aa8d73b0c85a2895bf091d2b9990b60524552dda")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'seturl.in')
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
 #---------------------------------------------------------------
